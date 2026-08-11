f11e5965ef063f90f8907c861ffa28c3168eabeb
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-11 16:03:39 -0500

    feat: make all pages work with navigation (connect pages with React Router)
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import CalculatorPage from "./pages/CalculatorPage";
+import FAQPage from "./pages/FAQPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
 function App() {
-  return (
-    <main>
-      <h1>Can I Afford This?</h1>
-      <p>Project setup complete.</p>
-    </main>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+
+                <Route
+                    path="/calculator"
+                    element={<CalculatorPage />}
+                />
+
+                <Route path="/faq" element={<FAQPage />} />
+
+                <Route
+                    path="/about"
+                    element={<AboutPage />}
+                />
+
+                <Route
+                    path="/contact"
+                    element={<ContactPage />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
