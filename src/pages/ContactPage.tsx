c3b74c1cdff77b5ce0f070a63442479f310b8ed7
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-11 12:58:51 -0500

    feat: create Contact page
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,15 @@
+function ContactPage() {
+    return (
+        <main>
+            <h1>Contact</h1>
+
+            <p>
+                Have feedback, questions, or ideas for improving the
+                affordability calculator? This page will provide a way
+                to get in touch.
+            </p>
+        </main>
+    );
+}
+
+export default ContactPage;
