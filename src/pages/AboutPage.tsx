b5214912be36d5b98e66b291fe902f31afdb9a9a
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-11 12:54:11 -0500

    feat: create About page
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,15 @@
+function AboutPage() {
+    return (
+        <main>
+            <h1>About This Calculator</h1>
+
+            <p>
+                This tool is designed to help people think beyond
+                whether they can simply pay for something and instead
+                understand the broader financial impact of a purchase.
+            </p>
+        </main>
+    );
+}
+
+export default AboutPage;
