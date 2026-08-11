2a5af7befe6fea80e2de33f6b91bb0f3f38da14a
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-11 12:38:14 -0500

    feat: create Home page
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+    return (
+        <main>
+            <h1>Can I Afford This?</h1>
+
+            <p>
+                Make smarter purchase decisions with a clearer
+                understanding of what you can truly afford.
+            </p>
+        </main>
+    );
+}
+
+export default HomePage;
