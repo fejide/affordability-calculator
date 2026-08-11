c4ca1b40ac38031cc8a422f31b931790863f9e87
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-11 12:41:45 -0500

    feat: create Calculator page
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

---
+++ src/pages/CalculatorPage.tsx
@@ -0,0 +1,14 @@
+function CalculatorPage() {
+    return (
+        <main>
+            <h1>Can I Afford This?</h1>
+
+            <p>
+                Enter your financial information and the purchase
+                you are considering to evaluate its true impact.
+            </p>
+        </main>
+    );
+}
+
+export default CalculatorPage;
