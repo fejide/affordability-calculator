53fa75e4438aeea10d0d0690dd5e3bb10bb4124c
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-13 13:15:14 -0500

    feat: add navbar to pages
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
 
 import HomePage from "./pages/HomePage";
 import CalculatorPage from "./pages/CalculatorPage";
@@ -10,24 +12,13 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-
-                <Route
-                    path="/calculator"
-                    element={<CalculatorPage />}
-                />
-
-                <Route path="/faq" element={<FAQPage />} />
-
-                <Route
-                    path="/about"
-                    element={<AboutPage />}
-                />
-
-                <Route
-                    path="/contact"
-                    element={<ContactPage />}
-                />
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/calculator" element={<CalculatorPage />} />
+                    <Route path="/faq" element={<FAQPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
