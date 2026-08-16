8b3094e2eae5102be91d69bb9466ec586d9041bc
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-08-15 19:00:08 -0500

    feat: add shared footer
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MainLayout() {
     return (
@@ -7,6 +8,8 @@ function MainLayout() {
             <Navbar />
 
             <Outlet />
+
+            <Footer />
         </>
     );
 }
